376c163a7e14d306990fb9608a90124af5ebc19b
Alexander Simnonov <aleksandrsimonov9622@gmail.com>
2024-10-08 20:05:05 +0300

    add new hello.pas

---
+++ hello.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var language := ReadInteger('Введите  язык, которым владеете(1 - Русский язык(rus), 2 - Английский язык(eng), 3 - Французский язык(fr): ');
+  Assert((language = 1) or (language = 2) or (language = 3));
+  case language of
+    1: println('Привет, мир!');
+    2: println('Hello, world!');
+    3: println('Bonjour le monde!');
+  end;
+end.
